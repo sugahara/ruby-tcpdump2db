923d7117224bee1600aba6d4a1b9f1c98115988e
Jun Sugahara <j.sghr.jp@gmail.com>
2012-05-22 10:09:40 +0900

    test

--- test.rb
+++ test.rb
@@ -1,2 +1,7 @@
-@filename = ARGV
-p @filename
+@db = Mysql::init()
+@db.options(Mysql::OPT_LOCAL_INFILE)
+@db.real_connect("133.101.57.201","ruby","suga0329","tcpdump")
+
+sql = "show tables from tcpdump like `test`"
+
+@db.query(sql)
