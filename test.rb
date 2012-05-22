461d2704e821b6e3ccccfeedbdad6d0510e5aca9
Jun Sugahara <j.sghr.jp@gmail.com>
2012-05-22 09:45:43 +0900

    add debug mess

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 io = IO.popen("lsof")
 io.each do |line|
-  p line
   if line.index("scpdump")
+    p line
     if line.split(" ").first == "scp"
       @uploading_file = line.split(" ").last
     end
