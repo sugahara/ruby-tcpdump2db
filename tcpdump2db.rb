0cf8cadaff95a36666c4195ebfc107bde6f9c3e5
Jun Sugahara <j.sghr.jp@gmail.com>
2012-05-22 09:40:37 +0900

    add uploading_file detection

--- tcpdump2db.rb
+++ tcpdump2db.rb
@@ -48,6 +48,17 @@ end
 @db.options(Mysql::OPT_LOCAL_INFILE)
 @db.real_connect("133.101.57.201","ruby","suga0329","tcpdump")
 
+
+io = IO.popen("lsof")
+io.each do |line|
+  if line.index("scpdump")
+    if line.split(" ").first == "scp"
+      @uploading_file = line.split(" ").last
+    end
+  end
+end
+
+
 @filename = ARGV
 @filename.each do |fn|
   @table_name = File::basename(fn)
---
+++ test.rb
@@ -0,0 +1,10 @@
+io = IO.popen("lsof")
+io.each do |line|
+  if line.index("scpdump")
+    if line.split(" ").first == "scp"
+      @uploading_file = line.split(" ").last
+    end
+  end
+end
+
+p @uploading_file
